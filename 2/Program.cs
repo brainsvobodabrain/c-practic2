699b17dced1eb40e558ea1ad21794644c28ee6e2
BraIN <brainsvoboda@mail.ru>
2023-01-18 15:57:43 +0300

    task2 complete

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,8 +1,2 @@
-//Task2
-System.Console.Write("Введите число: ");
-int chislo=Convert.ToInt32(Console.ReadLine());
-if (chislo.ToString().Length<3){
-    System.Console.WriteLine("Длинна числа <3");
-}else{
-    System.Console.WriteLine($"3-я цифра числа {chislo} = {chislo.ToString()[2]}");
-}
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
