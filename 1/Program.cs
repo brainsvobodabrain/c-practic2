68fb5f00a2121c2c24c8196ae79b96812321b4d3
BraIN <brainsvoboda@mail.ru>
2023-01-18 15:37:09 +0300

    task1 complete

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//TASK 1
+Random rand=new Random();
+int r=rand.Next(100,1000);
+System.Console.WriteLine($"Взято случайное число {r}");
+System.Console.WriteLine($"Вторая цифра числа взятая методом перевода в строку {r.ToString()[1]}");
+System.Console.WriteLine($"Вторая цифра числа взятая математическим методом {(r-(r/100*100)-r%10)/10}");
