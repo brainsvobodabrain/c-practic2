7e1a465005113668bea1fa62e99e196675a1951c
BraIN <brainsvoboda@mail.ru>
2023-01-18 16:11:46 +0300

    task3 complete

--- 3/Program.cs
+++ 3/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите день недели: ");
+int d=Convert.ToInt32(Console.ReadLine());
+if (d>5&&d<8){
+    System.Console.WriteLine("Да");
+}else{
+    System.Console.WriteLine("Нет");
+}
